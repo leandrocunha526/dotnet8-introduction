63e7dd1fea235d7df76d0dcfae643201c445d80f
Leandro Cunha <leandrocunha016@gmail.com>
2024-05-23 15:48:32 -0300

    feat: Add CategoryController with CRUD operations
    The code changes introduce a new `CategoryController` class with CRUD (Create, Read, Update, Delete) operations for managing categories. This controller is responsible for handling HTTP requests related to categories and interacting with the `IRepository` interface.
    
    The commit message follows the convention of starting with a type prefix (`feat` for feature) and provides a concise summary of the changes made.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,123 @@
+using dotnet8_introduction.Data;
+using Microsoft.AspNetCore.Mvc;
+using dotnet8_introduction.Model;
+
+namespace dotnet8_introduction.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : Controller
+    {
+        public IRepository? _repository { get; }
+        public CategoryController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("list")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await _repository!.GetAllCategoriesAsync();
+                return Ok(results);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                "Error 500 - Internal Server Error (database error)"
+                );
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var results = await _repository!.GetCategoryByIdAsync(id);
+                if (results == null)
+                {
+                    return NotFound();
+                }
+                return Ok(results);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                "Error 500 - Internal Server Error (database error)"
+                );
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Category model)
+        {
+            try
+            {
+                _repository!.Add(model);
+                if (await _repository.SaveChangesAsync())
+                {
+                    return Ok(model);
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                "Error 500 - Internal Server Error (database error)"
+                );
+            }
+            return BadRequest();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Category model)
+        {
+            try
+            {
+                var category = await _repository!.GetCategoryByIdAsync(id);
+                if (category == null)
+                {
+                    return NotFound();
+                }
+                _repository.Update(model);
+                if (await _repository.SaveChangesAsync())
+                {
+                    return Ok(model);
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                "Error 500 - Internal Server Error (database error)"
+                );
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var category = await _repository!.GetCategoryByIdAsync(id);
+                if (category == null)
+                {
+                    return NotFound();
+                }
+                _repository.Delete(category);
+                if (await _repository.SaveChangesAsync())
+                {
+                    return Ok(new { message = "Category deleted" });
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                "Error 500 - Internal Server Error (database error)"
+                );
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,139 @@
+using dotnet8_introduction.Data;
+using dotnet8_introduction.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet8_introduction.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        public IRepository? _repository { get; }
+        public ProductController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("list")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await _repository!.GetAllProductsAsync(includeCategory: true);
+                return Ok(results);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                "Error 500 - Internal Server Error (database error)"
+                );
+            }
+        }
+        [HttpGet("average-price")]
+        public async Task<ActionResult> GetAveragePrice()
+        {
+            var averagePrice = await _repository!.GetAveragePriceAsync();
+
+            if (averagePrice == 0)
+            {
+                return Ok(new { AveragePrice = averagePrice, Message = "No products available." });
+            }
+
+            return Ok(new { AveragePrice = averagePrice, Message = "Average price calculated successfully." });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var results = await _repository!.GetProductByIdAsync(id, includeCategory: true);
+                if (results == null)
+                {
+                    return NotFound();
+                }
+                return Ok(results);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                "Error 500 - Internal Server Error (database error)"
+                );
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Product model)
+        {
+            try
+            {
+                _repository!.Add(model);
+                if (await _repository.SaveChangesAsync())
+                {
+                    return Ok(model);
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                "Error 500 - Internal Server Error (database error)"
+                );
+            }
+            return BadRequest();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Product model)
+        {
+            try
+            {
+                var product = await _repository!.GetProductByIdAsync(id, includeCategory: false);
+                if (product == null)
+                {
+                    return NotFound();
+                }
+
+                _repository.Update(model);
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    return Ok(model);
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                "Error 500 - Internal Server Error (database error)"
+                );
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var product = await _repository!.GetProductByIdAsync(id, includeCategory: false);
+                if (product == null)
+                {
+                    return NotFound();
+                }
+
+                _repository.Delete(product);
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    return Ok(new { message = "Product deleted successfully." });
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                "Error 500 - Internal Server Error (database error)"
+                );
+            }
+            return BadRequest();
+        }
+    }
+}

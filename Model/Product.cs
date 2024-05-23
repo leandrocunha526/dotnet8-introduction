003c9c539653ab74e41bb48bcbd997fd97bbb028
Leandro Cunha <leandrocunha016@gmail.com>
2024-05-23 15:50:02 -0300

    chore: Update Product.cs and dotnet8-introduction.csproj
    The code changes update the `Product.cs` file by making the `Name`, `Description`, and `Category` properties nullable. Additionally, the `dotnet8-introduction.csproj` file is updated to enable nullable reference types.
    
    This commit message follows the convention of starting with a type prefix (`chore` for chore) and provides a concise summary of the changes made.

--- Model/Product.cs
+++ Model/Product.cs
@@ -1,4 +1,3 @@
-using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace dotnet8_introduction.Model
@@ -9,15 +8,15 @@ namespace dotnet8_introduction.Model
         public int Id { get; set; }
 
         [Required]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         [Required]
         public float Price { get; set; }
 
         [Required]
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         [Required]
-        public Category category { get; set; }
+        public Category? category { get; set; }
     }
 }

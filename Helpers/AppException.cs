5ee854ba8e6dab3180dba0ebe94dacf351c3f6a5
Leandro Cunha <leandrocunha016@gmail.com>
2024-05-25 14:33:32 -0300

    feat: Add AppException, AppSettings, and AutoMapper
    The code changes add new classes `AppException`, `AppSettings`, and `AutoMapperProfile` to the `Helpers` namespace. `AppException` is a custom exception class, `AppSettings` is a class for storing application settings, and `AutoMapperProfile` is a class for configuring AutoMapper mappings between entities and models.
    
    This commit message follows the convention of starting with a type prefix (`feat` for feature) and provides a concise summary of the changes made.

---
+++ Helpers/AppException.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Globalization;
+
+namespace dotnet8_introduction.Helpers
+{
+	public class AppException : Exception
+	{
+		public AppException() : base() { }
+		public AppException(string message) : base(message) { }
+		public AppException(string message, params object[] args) : base(string.Format(CultureInfo.CurrentCulture, message, args)) { }
+	}
+}
---
+++ Helpers/AppSettings.cs
@@ -0,0 +1,7 @@
+namespace dotnet8_introduction.Helpers
+{
+    public class AppSettings
+    {
+        public string? Secret { get; set; }
+    }
+}
